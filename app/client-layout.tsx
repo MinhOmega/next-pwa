c9dda1441f86a80327ff9b3d1473341c806269ea
MinhOmega <minh.vo@orientsoftware.com>
2025-05-20 13:53:59 +0700

    feat: initial PWA

---
+++ app/client-layout.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useEffect } from 'react'
+
+export default function ClientLayout({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js')
+        .then((registration) => console.log('Service Worker registered'))
+        .catch((error) => console.log('Service Worker registration failed:', error))
+    }
+  }, [])
+
+  return <>{children}</>
+}
