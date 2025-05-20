c3cc3de91c97e6dcac5f3a7d9e692238fbf77949
MinhOmega <minh.vo@orientsoftware.com>
2025-05-20 14:03:27 +0700

    feat: update manifest with screenshots and formatting improvements

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,27 +1,47 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'My Next.js PWA',
-    short_name: 'MyPWA',
-    description: 'A Progressive Web App built with Next.js 15',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#000000',
+    name: "My Next.js PWA",
+    short_name: "MyPWA",
+    description: "A Progressive Web App built with Next.js 15",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
     icons: [
       {
-        "src": "/web-app-manifest-192x192.png",
-        "sizes": "192x192",
-        "type": "image/png",
-        "purpose": "maskable"
+        src: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
       },
       {
-        "src": "/web-app-manifest-512x512.png",
-        "sizes": "512x512",
-        "type": "image/png",
-        "purpose": "maskable"
-      }
+        src: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
     ],
-  }
+    screenshots: [
+      {
+        src: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        form_factor: "narrow",
+      },
+      {
+        src: "/web-app-manifest-192x192.png",
+        type: "image/png",
+        sizes: "2460x1674",
+        form_factor: "wide",
+      },
+      {
+        src: "/web-app-manifest-192x192.png",
+        type: "image/png",
+        sizes: "2460x1674",
+        form_factor: "wide",
+      },
+    ],
+  };
 }
