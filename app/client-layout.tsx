8c0420229b92cefa36369a7f2a7ce0270984065c
MinhOmega <minh.vo@orientsoftware.com>
2025-05-28 12:58:35 +0700

    feat: add PWA installation support and update dependencies
    - Introduced a new ClientLayout component to handle PWA installation prompts.
    - Updated package.json to include @khmyznikov/pwa-install and adjusted dependencies.
    - Modified pnpm-lock.yaml to reflect new package versions and dependencies.
    - Integrated ClientLayout into the main layout for PWA functionality.

---
+++ app/client-layout.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import PWAInstall from '@khmyznikov/pwa-install/react-legacy';
+import { PWAInstallElement } from '@khmyznikov/pwa-install';
+import { useEffect, useRef, useState } from 'react';
+
+const appName = 'My App';
+
+declare global {
+  interface Window {
+    promptEvent: any;
+  }
+}
+
+const ClientLayout = ({ children }: { children: React.ReactNode }) => {
+  const [promptEvent, setPromptEvent] = useState(null);
+  const pwaInstallRef = useRef<PWAInstallElement>(null);
+
+  // externalPromptEvent is only if your app is big and slow to start
+  // check index.html for additional code.
+  // https://github.com/khmyznikov/pwa-install?tab=readme-ov-file#async-mode
+  useEffect(() => {
+    let lastPromptEvent = window.promptEvent;
+
+    const intervalId = setInterval(() => {
+      if (window.promptEvent !== lastPromptEvent) {
+        lastPromptEvent = window.promptEvent;
+        setPromptEvent(window.promptEvent);
+      }
+    }, 100);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <div>
+      {children}
+      <PWAInstall
+        ref={pwaInstallRef}
+        name={appName}
+        icon={'/next.svg'}
+        externalPromptEvent={promptEvent}
+        onPwaInstallAvailableEvent={(event) => console.log(event)}
+      />
+    </div>
+  );
+};
+
+export default ClientLayout;
